1f6d8071c9b59ec7754326994f92d68c9a5da2e9
Pavel Kononov <pro100cuber@gmail.com>
2023-09-02 14:43:16 +0700

    test: opeartor overloading

--- extension/test.c
+++ extension/test.c
@@ -9,22 +9,39 @@ ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_INFO_EX(arginfo_multiply, 0, 0, 0)
 ZEND_END_ARG_INFO()
-
 const zend_function_entry test_functions[] = {
 	PHP_FE(test, arginfo_test)
 		PHP_FE(multiply, arginfo_multiply)
 			PHP_FE_END};
 
+static int op_handler(zend_execute_data *execute_data)
+{
+	// printf("hello here");
+	zend_op *opline = execute_data->opline;
+	zval *op1 = EX_VAR(opline->result.var);
+	ZVAL_LONG(op1, 5);
+	execute_data->opline++;
+	return ZEND_USER_OPCODE_CONTINUE;
+}
+static PHP_MINIT_FUNCTION(test)
+{
+	zend_set_user_opcode_handler(ZEND_ADD, op_handler);
+	zend_set_user_opcode_handler(ZEND_SUB, op_handler);
+	zend_set_user_opcode_handler(3, op_handler);
+	zend_set_user_opcode_handler(4, op_handler);
+	// printf("hello world");
+}
+
 zend_module_entry test_module_entry = {
 	STANDARD_MODULE_HEADER, // #if ZEND_MODULE_API_NO >= 20010901
 	"test",					// название модуля
 	test_functions,			// указываем экспортируемые функции
-	NULL,					// PHP_MINIT(test), Module Initialization
+	PHP_MINIT(test),		// PHP_MINIT(test), Module Initialization
 	NULL,					// PHP_MSHUTDOWN(test), Module Shutdown
 	NULL,					// PHP_RINIT(test), Request Initialization
 	NULL,					// PHP_RSHUTDOWN(test), Request Shutdown
 	NULL,					// PHP_MINFO(test), Module Info (для phpinfo())
-	"0.1",					// версия нашего модуля
+	"0.3",					// версия нашего модуля
 	STANDARD_MODULE_PROPERTIES};
 
 ZEND_GET_MODULE(test)
@@ -45,12 +62,19 @@ PHP_FUNCTION(multiply)
 		return;
 	}
 	double sum = 0;
-	for(int i =0; i < num_varargs;i++) {
-		if(varargs[i].u1.type_info == IS_DOUBLE ) {
+	for (int i = 0; i < num_varargs; i++)
+	{
+		if (varargs[i].u1.type_info == IS_DOUBLE)
+		{
 			sum += varargs[i].value.dval;
 		}
-		if(varargs[i].u1.type_info == IS_LONG) {
-			sum+= varargs[i].value.lval;
+		if (varargs[i].u1.type_info == IS_LONG)
+		{
+			sum += varargs[i].value.lval;
+		}
+		if (varargs[i].u1.type_info == IS_STRING)
+		{
+			sum += 10;
 		}
 	}
 	RETURN_DOUBLE(sum);
--- test.php
+++ test.php
@@ -1,6 +1,7 @@
 <?php
-
-
+opcache_reset();
+$a = 1;
+echo $a + 1;
 echo test();
 
-echo multiply(1,2,3,4,5,6,7,8.5);
+echo multiply(1, 2, 3, 4, 5, 6, 7, 8.5, '1');
