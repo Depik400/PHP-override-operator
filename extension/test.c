eb187f8265e1060c1be168330f6ef53cde2ba89c
Pavel Kononov <pro100cuber@gmail.com>
2023-09-02 16:32:40 +0700

    ✨ object op handler

--- extension/test.c
+++ extension/test.c
@@ -14,22 +14,97 @@ const zend_function_entry test_functions[] = {
 		PHP_FE(multiply, arginfo_multiply)
 			PHP_FE_END};
 
+zend_string *operator_method_name(zend_uchar opcode)
+{
+	switch (opcode)
+	{
+	case ZEND_ADD:
+		return zend_string_init("__add", strlen("__add"), 1);
+
+	default:
+		return zend_string_init("__any_action", strlen("__any_action"), 1);
+	}
+}
+
+zend_bool operator_get_method(zend_string *method, zval *obj,
+							  zend_fcall_info *fci,
+							  zend_fcall_info_cache *fcc)
+{
+	memset(fci, 0, sizeof(zend_fcall_info));
+	fci->size = sizeof(zend_fcall_info);
+	fci->object = Z_OBJ_P(obj);
+	ZVAL_STR(&(fci->function_name), method);
+
+	if (!zend_is_callable_ex(&(fci->function_name), fci->object,
+							 IS_CALLABLE_CHECK_SILENT,
+							 NULL, fcc, NULL))
+	{
+		return 0;
+	}
+	/* Disallow dispatch via __call */
+	if (fcc->function_handler == Z_OBJCE_P(obj)->__call)
+	{
+		return 0;
+	}
+	if (fcc->function_handler->type == ZEND_USER_FUNCTION)
+	{
+		zend_op_array *oparray = (zend_op_array *)(fcc->function_handler);
+		if (oparray->fn_flags & ZEND_ACC_CALL_VIA_TRAMPOLINE)
+		{
+			return 0;
+		}
+	}
+
+	return 1;
+}
 static int op_handler(zend_execute_data *execute_data)
 {
 	// printf("hello here");
 	zend_op *opline = execute_data->opline;
 	zval *op1 = EX_VAR(opline->result.var);
+	zend_fcall_info fci;
+	zend_fcall_info_cache fcc;
+	// zend_string *method = operator_method_name(opline->opcode);
 	ZVAL_LONG(op1, 5);
 	execute_data->opline++;
 	return ZEND_USER_OPCODE_CONTINUE;
 }
+
+static int op_handler_object(zend_execute_data *execute_data)
+{
+	// printf("hello here");
+	zend_op *opline = execute_data->opline;
+	zval *op1 = EX_VAR(opline->op1.var);
+	zval *op2 = EX_VAR(opline->op2.var);
+	zend_fcall_info fci;
+	zend_fcall_info_cache fcc;
+	zend_string *method = operator_method_name(opline->opcode);
+	if (Z_TYPE_P(op1) != IS_OBJECT || !operator_get_method(method, op1, &fci, &fcc))
+		return ZEND_USER_OPCODE_DISPATCH;
+
+	fci.retval = EX_VAR(opline->result.var);
+	fci.params = op2;
+	fci.param_count = op2 ? 1 : 0;
+	if (FAILURE == zend_call_function(&fci, &fcc))
+	{
+		php_error(
+			E_WARNING,
+			"Failed calling %s::%s()",
+			Z_OBJCE_P(op1)->name,
+			Z_STRVAL(fci.function_name));
+		ZVAL_NULL(fci.retval);
+	}
+	EX(opline) = opline + 1;
+	return ZEND_USER_OPCODE_CONTINUE;
+}
+
 static PHP_MINIT_FUNCTION(test)
 {
-	zend_set_user_opcode_handler(ZEND_ADD, op_handler);
-	zend_set_user_opcode_handler(ZEND_SUB, op_handler);
-	zend_set_user_opcode_handler(3, op_handler);
-	zend_set_user_opcode_handler(4, op_handler);
-	// printf("hello world");
+	zend_set_user_opcode_handler(ZEND_ADD, op_handler_object);
+	// zend_set_user_opcode_handler(ZEND_SUB, op_handler);
+	//  zend_set_user_opcode_handler(3, op_handler);
+	//  zend_set_user_opcode_handler(4, op_handler);
+	//  printf("hello world");
 }
 
 zend_module_entry test_module_entry = {
--- test.php
+++ test.php
@@ -5,3 +5,16 @@ echo $a + 1;
 echo test();
 
 echo multiply(1, 2, 3, 4, 5, 6, 7, 8.5, '1');
+
+class A
+{
+    private $a = 1;
+    public function __add($s)
+    {
+        return $this->a + $s;
+    }
+}
+
+$a = new A();
+
+echo '   ' . ($a + 1) . '    ';
